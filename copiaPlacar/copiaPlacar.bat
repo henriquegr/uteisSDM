9dca37514fbff8f65f42f9e902b420436d3fa637
Henrique Grammelsbacher <henrique@interadapt.com.br>
2013-02-01 00:19:35 -0200

    Tratamento de erro e logs de mensagem

--- copiaPlacar/copiaPlacar.bat
+++ copiaPlacar/copiaPlacar.bat
@@ -1,23 +1,44 @@
 @echo off
+@cls
 
-SET NOMEBKP=User_Query_%date:~-4,4%%date:~-10,2%%date:~-7,2%_%time:~0,2%%time:~3,2%%time:~6,2%
-
-@pdm_extract User_Query > %NOMEBKP%.bkp
-
+@echo:
+@echo:
+@echo ==============================================================================
+@echo Pegando os IDs que interessam 
 @bop_odump domsrvr role "name like '%~1'" persistent_id | grep role | sed "s/ .*//g" > origem.tmp
 @set /p origem=<ORIGEM.tmp
-@echo ID de Origem:  %ORIGEM% - %~1
+@echo ID de Origem:  '%ORIGEM%' - %~1
 
 bop_odump domsrvr role "name like '%~2'" persistent_id | grep role | sed "s/ .*//g" > destino.tmp
 @set /p DESTINO=<destino.tmp
-@echo ID de destino: %DESTINO% - %~2
+@echo ID de destino: '%DESTINO%' - %~2
 
+IF [%DESTINO%] == []  goto :ERRO
+IF [%ORIGEM%] == [] goto :ERRO
+
+
+@echo:
+@echo:
+@echo ==============================================================================
+@echo Fazendo Backup da User Query
+SET NOMEBKP=User_Query_%date:~-4,4%%date:~-10,2%%date:~-7,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+
+@pdm_extract User_Query > "%NOMEBKP%.bkp"
+
+@echo:
+@echo:
+@echo ==============================================================================
+@echo Criando os arquivos base para o samba-lele
 @pdm_extract -f"Select * from User_Query where obj_persid='%ORIGEM%'" > origem.bkp
 @pdm_extract -f"Select * from User_Query where obj_persid='%DESTINO%'" > destino.bkp
 
 @pdm_extract -f"Select sequence, parent from User_Query where obj_persid = '%ORIGEM%' and parent>0" > pais_origem.tmp
 @pdm_extract -f"Select sequence, parent from User_Query where obj_persid = '%DESTINO%' and parent>0" > pais_destino.tmp
 
+@echo:
+@echo:
+@echo ==============================================================================
+@echo Executando a copia
 @type modelo.deref | sed "s/rule.*/rule = \"SELECT sequence FROM User_Query WHERE id=? and obj_persid='%ORIGEM%'\"/g" > destino_deref.tmp
 @pdm_deref -s destino_deref.tmp  pais_origem.tmp > pais_para_deref.tmp
 
@@ -33,7 +54,28 @@ bop_odump domsrvr role "name like '%~2'" persistent_id | grep role | sed "s/ .*/
 @pdm_deref -s destino2_deref.tmp pais_para_deref.tmp > pais_userload.tmp
 @pdm_userload -f pais_userload.tmp
 
-del *.tmp
+@echo:
+@echo:
+@echo ==============================================================================
+@echo Limpando a zona
+@echo:
+@echo:
+@echo FEITO!!!!!
 
+
+del *.tmp
+goto :END
+
+:ERRO
+@echo:
+@echo:
+@echo ==============================================================================
+@Echo ERRO!!!! - Nao encontrei um dos IDs. Olha direito o nome!!!
+@echo ==============================================================================
+@echo:
+@echo:
+
+
+:END
 
 
